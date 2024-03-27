b4f08f5a41a1f8bdcf48a6532e276fd2bf5de846
kanggeonnim <gun4329@gmail.com>
2024-03-27 17:29:36 +0900

    Update config.py to include GPT_PROMPT_KEYWORD, IMAGE_FILE_PATH, AWS_CLOUD_FRONT, and DB_PORT configurations.

--- app/config/config.py
+++ app/config/config.py
@@ -13,11 +13,12 @@ class Settings:
     YOUTUBE_API_KEY: str = config_secret_common["YOUTUBE_API_KEY"]
     GPT_PROMPT_BILL: str = config_secret_common["GPT_PROMPT_BILL"]
     GPT_PROMPT_ISSUE: str = config_secret_common["GPT_PROMPT_ISSUE"]
-    GPT_PROMPT_NEWS: str = config_secret_common["GPT_PROMPT_NEWS"]
+    GPT_PROMPT_KEYWORD: str = config_secret_common["GPT_PROMPT_KEYWORD"]
     VIDEO_FILE_PATH: str = config_secret_common["video_file_path"]
     AUDIO_FILE_PATH: str = config_secret_common["audio_file_path"]
     SCRIPT_FILE_PATH: str = config_secret_common["script_file_path"]
     CLIP_FILE_PATH: str = config_secret_common["clip_file_path"]
+    IMAGE_FILE_PATH: str = config_secret_common["image_file_path"]
     AWS_ACCESS_KEY_ID: str = config_secret_common["AWS_ACCESS_KEY_ID"]
     AWS_SECRET_ACCESS_KEY: str = config_secret_common["AWS_SECRET_ACCESS_KEY"]
     AWS_DEFAULT_REGION: str = config_secret_common["AWS_DEFAULT_REGION"]
@@ -25,6 +26,8 @@ class Settings:
     DB_USER: str = config_secret_common["DB_USER"]
     DB_PASSWORD: str = config_secret_common["DB_PASSWORD"]
     DB_DATABASE: str = config_secret_common["DB_DATABASE"]
+    AWS_CLOUD_FRONT: str = config_secret_common["AWS_CLOUD_FRONT"]
+    DB_PORT: str = config_secret_common["DB_PORT"]
 
 
 settings = Settings()
