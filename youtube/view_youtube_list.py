037e7e831da45a28c859622b0bcc146e257f6a70
kanggeonnim <gun4329@gmail.com>
2024-03-27 17:29:44 +0900

    Refactor youtube video list retrieval logic and add functionality to save nextPageToken in a log file.

--- youtube/view_youtube_list.py
+++ youtube/view_youtube_list.py
@@ -13,6 +13,7 @@ youtube_api_key = settings.YOUTUBE_API_KEY
 def get_youtube_list():
     # 가져올 채널의 ID를 입력합니다.
     channel_id = 'UCL-WOj1FxKR8Hlzg5tvnWKg'
+    playlist_id = 'PLA_P66SgTXS0NKjRM6XcUtFBQVLZLlIXz'
     #   국회방송NATV
 
     # YouTube Data API 클라이언트를 빌드합니다.
@@ -22,33 +23,51 @@ def get_youtube_list():
 
     try:
         # 동영상 목록을 가져옵니다.
-        request = youtube.search().list(
-            part='id,snippet',
-            channelId=channel_id,
-            order='date',
-            type='video',
-            maxResults=20
+        # request = youtube.playlistItems().list(
+        #     part='id,snippet',
+        #     channelId=channel_id,
+        #     order='date',
+        #     type='video',
+        #     maxResults=50
+        # )
+
+        request = youtube.playlistItems().list(
+            part='snippet',
+            playlistId=playlist_id,
+            maxResults=50
         )
         response = request.execute()
-        # print(response)
+        nextPageToken = response['nextPageToken']
+
+        # save scripts
+        f = open('./log/token.txt', "a", encoding="utf-8")
+        f.write(str(datetime.today()) + '\n')
+        f.write(str(nextPageToken) + '\n')
+        f.write('------------------------\n')
+        f.close()
 
         videos = list()
         # 동영상 제목과 ID를 출력합니다.
         for item in response['items']:
             video_title = item['snippet']['title']
-            published_at = item['snippet']['publishTime'][0:10]
-            today = (datetime.today() - timedelta(days=1)).strftime("%Y-%m-%d")
-            live_broadcast = item['snippet']['liveBroadcastContent']
-            tumblr_url = item['snippet']['thumbnails']['high']
-            video_id = item['id']['videoId']
+            # published_at = item['snippet']['publishTime'][0:10]
+            published_at = item['snippet']['publishedAt'][0:10]
+            today = (datetime.today() - timedelta(days=11)).strftime("%Y-%m-%d")
+            print(f'today is {today} and published at {published_at}')
+            print(f'{video_title}')
 
-            if (today == published_at and live_broadcast == "none"):
-                # if (today != published_at and live_broadcast == "none"):
-                videos.append(video_id)
-                # print(live_broadcast)
-                # print(f'{video_title}\n{video_id}\n{tumblr_url}')
-                # print(published_at)
-                # print("==============================")
+            # live_broadcast = item['snippet']['liveBroadcastContent']
+            tumblr_url = item['snippet']['thumbnails']['high']
+            # video_id = item['id']['videoId']
+            video_id = item['snippet']['resourceId']['videoId']
+
+            # if today == published_at and live_broadcast == "none":
+
+            videos.append(video_id)
+            # print(live_broadcast)
+            # print(f'{video_title}\n{video_id}\n{tumblr_url}')
+            # print(published_at)
+            # print("==============================")
         print(videos)
 
         if len(videos) == 0:
@@ -61,3 +80,7 @@ def get_youtube_list():
 
     except HttpError as e:
         print(f'An HTTP error {e.resp.status} occurred:\n{e.content}')
+
+
+if __name__ == '__main__':
+    get_youtube_list()
