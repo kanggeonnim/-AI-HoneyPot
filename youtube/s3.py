d18814e5cb13c4aba6c059c98bae9559e31d21f0
kanggeonnim <gun4329@gmail.com>
2024-04-01 16:15:08 +0900

    Fixed an issue where category ID was not assigned correctly if it did not exist, now assigning it as "1" and inserting keywords accordingly. Also added a function to fix JSON syntax errors in keyword files.

--- youtube/s3.py
+++ youtube/s3.py
@@ -294,7 +294,14 @@ def upload_s3():
                             for key, value in dictionary_data.items():
                                 category_id = find_category_id_by_category_name(connection, value)
                                 # print(f'Key: {key}, Value: {value} for category: {category_id}')
+
+                                # 카테고리 아이디가 존재하지 않는다면 기타로 분류
+                                if not category_id:
+                                    category_id = 1
+
+                                # 키워드 카테고리 부여
                                 insert_keyword(connection, video_id, key, category_id)
+
                         # 연결 종료
                         connection.close()
                         print("MySQL connection closed.")
@@ -302,16 +309,54 @@ def upload_s3():
                 print(f"An error occurred: {e}")
 
 
+def keyword_fix():
+    connection = connect_to_mysql()
+    # KEYWORD 파일 열기
+    with open("whisper/script/[KEYWORD]한동훈 국민의힘 비상대책위원장 취임 한 달.txt", "r", encoding="utf-8") as file:
+        file_contents = file.read()
+
+    video_id = find_video_id_by_video_name(connection, "한동훈 국민의힘 비상대책위원장 취임 한 달")
+
+    if video_id:
+        # 키워드 속성 추출 : {}로 감싸진 부분 추출
+        start_index = file_contents.find('{')
+        end_index = file_contents.rfind('}')
+        extracted_part = file_contents[start_index:end_index + 1]
+        # dictionary 형식으로 변환
+        try:
+            dictionary_data = eval(extracted_part)
+        except Exception as e:
+
+            # JSON 파싱 실패 시, 문법 오류를 수정하여 JSON 파일로 변환
+            fixed_content = file_contents.strip()[:-4] + ",\n" + file_contents.strip()[-3]
+            # fixed_content = fixed_content.strip()[:-3] + ",\n" + fixed_content.strip()[-2]
+            print(fixed_content)
+            # 수정된 내용을 파일에 다시 쓰기
+            with open("whisper/script/[KEYWORD]한동훈 국민의힘 비상대책위원장 취임 한 달.txt", 'w', encoding='utf-8') as file:
+                file.write(fixed_content)
+            print(f"파일이(가) 수정되었습니다.")
+
+        # 키워드 저장
+        for key, value in dictionary_data.items():
+            category_id = find_category_id_by_category_name(connection, value)
+            # print(f'Key: {key}, Value: {value} for category: {category_id}')
+            insert_keyword(connection, video_id, key, category_id)
+    # 연결 종료
+    connection.close()
+    print("MySQL connection closed.")
+
+
 if __name__ == "__main__":
     # print(get_video_time("../youtube/whisper/clip_video/철도지화화 경쟁.mp4"))
 
     # 키워드 카테고리 테이블 초기화.
     # init_keyword_category_table()
-    upload_s3()
+    # upload_s3()
     # unique_id = generate_unique_id()
     # generate_thumbnail(f'./whisper/clip_video/민생 공약과 선거 전략.mp4', f'./whisper/image/{unique_id}.jpg', 10)  # 10초 시점의 썸네일 생성
     #
     # image_upload_successful = upload_file_to_s3(f'./whisper/image/{unique_id}.jpg', 'yeouido-honeypot',
     #                                             f'images/{unique_id}.jpg')
 
+    keyword_fix()
     # generate_thumbnail("./whisper/clip_video/민생 공약과 선거 전략.mp4", generate_unique_id() + ".jpg", 10)  # 10초 시점의 썸네일 생성
